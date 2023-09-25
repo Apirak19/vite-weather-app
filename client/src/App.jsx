6acce2a3232c25b9899199c3e72f7687d96bd245
Apirak <apirakfakin@gmail.com>
2023-09-25 16:17:43 +0700

    backend added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import SignIn from "./components/auth/SignIn";
-import SignUp from "./components/auth/SignUp";
 import AuthDetails from "./components/auth/AuthDetails";
 
-const express = require("express");
-const fs = require('fs')
-const app = express();
-const url = require("url");
-const http = require("http");
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [backendData, setBackendData] = useState({users: []});
 
-
-  const homepage = fs.readFile((err, data) => {
-    if (err) {
-      console.error(err);
-    }
-    
-  }, )
-  const server = http.createServer((req, res) => {
-    if (req.url === "/"){
-      res.end()
-    }
-  }, )
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+    console.log(data);
+  }, []);
 
   return (
     <section className="">
-      <AuthDetails />
-      <p className="mt-10">All rights reserved to the owner of this website</p>
+      {typeof backendData.users === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+          backendData.users.map((user, i) => {
+          return <p key={i}>{user}</p>;
+        })
+      )}
     </section>
   );
 }
